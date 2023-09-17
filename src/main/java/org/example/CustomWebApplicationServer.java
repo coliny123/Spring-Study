7c629b338a122dcbcbfac307b4db7865271cfb2f
dohwan <dh990921@naver.com>
2023-09-17 18:53:03 +0900

    feat: Add Thread Pool

--- src/main/java/org/example/CustomWebApplicationServer.java
+++ src/main/java/org/example/CustomWebApplicationServer.java
@@ -8,18 +8,16 @@ import org.slf4j.Logger;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CustomWebApplicationServer {
-    /*
-        httprequest와 httpresponse의 임의 세팅 이유 : 프로토콜에 맞게 request 객체 생성, response 객체 생성해서 client와 server 간의 통신을 구현
-     */
-
-    // WebAppServer니까 port 가짐
     private final int port;
 
+    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
+
     // 로그 추가
-    private static final Logger logger = (Logger) LoggerFactory.getLogger(CustomWebApplicationServer.class);
+    private static final Logger logger = LoggerFactory.getLogger(CustomWebApplicationServer.class);
 
     public CustomWebApplicationServer(int port) {
         this.port = port;
@@ -32,11 +30,10 @@ public class CustomWebApplicationServer {
             Socket clientSocket;
             logger.info("[CustomWebApplicationServer] waiting for client.");    // client 생성될 때 log남기기
 
-            // serverSocket이 accept()로 client를 기다리게 함, client가 들어오면 clientSocket이 만들어져서 while문 안으로 들어옴
             while ((clientSocket = serverSocket.accept()) != null) {
                 logger.info("[CustomWebApplicationServer] client connected");   // 연결되면 log남기기
-                // step2 - 사용자 요청이 들어올 때마다 Thread를 새로 생성해서 사용자 요청을 처리하도록 한다.
-                new Thread(new ClientRequestHandler(clientSocket)).start();
+                // step3 - Thread Pool을 적용해 안정적인 서비스가 가능하도록 한다.
+                executorService.execute(new ClientRequestHandler(clientSocket));
             }
         }
     }
